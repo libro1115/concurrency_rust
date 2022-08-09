c0bf3366e77dea3d2ddcbddaf88be516b02034b7
=libro1115 <libro.1115@gmail.com>
2022-08-09 19:19:44 +0900

    メッセージの送受信

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, sync::mpsc};
 fn main() {
+    //concurrncy_test_normal();
+    
+    let (tx,rx) = mpsc::channel();
+    thread::spawn(move ||{
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+            ];
+        for val in vals{
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    for receive in rx{
+        println!("Got: {}",receive);
+    }
+}
+
+fn concurrncy_test_normal()
+{
     let handle = thread::spawn(||{
         for i in 1..10{
-            println!("number {}from the spawned thread", i);
+            println!("number {} from the spawned thread", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
