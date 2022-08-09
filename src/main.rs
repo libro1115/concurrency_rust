6a41b8963262ff042d1652fb1fc8360a70ccd3b6
=libro1115 <libro.1115@gmail.com>
2022-08-09 17:14:04 +0900

    並列動作

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{thread, time::Duration};
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(||{
+        for i in 1..10{
+            println!("number {}from the spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5{
+        println!("number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
